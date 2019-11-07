4384cab709636a8889620b77255f25b2869a35b8
John Kaser <20.john.kaser@ksd.org>
2019-11-07 12:03:16 -0800

    canvas

---
+++ Assets/Scripts/NavMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMove : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject player;
+    public float chaseDistance = 10;
+    private Vector3 home;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        home = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        if(direction.magnitude <= chaseDistance)
+        {
+            agent.destination = player.transform.position;
+        }
+        else
+        {
+            agent.destination = home;
+        }
+    }
+}
