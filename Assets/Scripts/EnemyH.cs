cdc21ec857b55581bc07ed9ca37d559e59d95ad4
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2019-11-07 12:16:30 -0800

    Made enemy script

---
+++ Assets/Scripts/EnemyH.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyH : MonoBehaviour
+{
+    public Collider MainCollider;
+    public Collider[] AllColliders;
+    private void Awake()
+    {
+        MainCollider = GetComponent<Collider>();
+        AllColliders = GetComponentsInChildren<Collider>(true);
+        DoRagdoll(false);
+
+    }
+
+   public void DoRagdoll(bool isRagdoll)
+    {
+        foreach (var col in AllColliders)
+            col.enabled = isRagdoll;
+        MainCollider.enabled = !isRagdoll;
+        GetComponent<Rigidbody>().useGravity = !isRagdoll;
+        GetComponent<Animator>().enabled = !isRagdoll;
+    }
+}
