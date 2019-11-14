1958696c933b8d93f2a45561df08ee83351a2d9e
John Kaser <20.john.kaser@ksd.org>
2019-11-14 11:46:43 -0800

    points

--- Assets/Scripts/EnemyHp.cs
+++ Assets/Scripts/EnemyHp.cs
@@ -8,12 +8,19 @@ public class EnemyHp : MonoBehaviour
 {
     public int health = 5;
     public Animator Enenmy;
+    public GameObject Player;
 
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Damage")
+        {
             health--;
-
+            Player.GetComponent<PlayerPoints>().points += 10;
+        }
     }
     private void Update()
     {
