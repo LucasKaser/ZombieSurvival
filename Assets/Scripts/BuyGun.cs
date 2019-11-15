24251b6fab64d838caaa823747992b317d0473d6
John Kaser <20.john.kaser@ksd.org>
2019-11-15 13:32:27 -0800

    bruh

---
+++ Assets/Scripts/BuyGun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyGun : MonoBehaviour
+{
+    public GameObject Gun;
+    public int cost;
+    public int playerpoints;
+    public GameObject Player;
+    private OVRGrabbable ovrgrabbable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrgrabbable = GetComponent<OVRGrabbable>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerpoints = Player.GetComponent<PlayerPoints>().points;
+        if (ovrgrabbable.isGrabbed && playerpoints >= cost)
+        {
+            Instantiate(Gun, transform.position, Quaternion.identity);
+        }
+    }
+}
