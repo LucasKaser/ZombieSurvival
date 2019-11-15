ed61fad33763c05eca47968d1f144f1454979283
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2019-11-15 12:05:40 -0800

    zombie anims

---
+++ Assets/Animation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{ 
+    private Animator Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Animator.SetBool("Attacking", true);
+            Debug.Log("Ligma moment"); 
+
+        }
+        
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+         if (collision.gameObject.tag == "Player")
+        {
+            Animator.SetBool("Attacking", true);
+            Debug.Log("Bruh moment");
+        }
+    }
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Animator.SetBool("Attacking", false);
+            Debug.Log("H moment");
+        }
+
+    }
+    
+
+
+}
---
+++ Assets/Scripts/AnimatorChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorChanger : MonoBehaviour
+{
+    private Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if(collision.gameObject.tag == "Player")
+        {
+            Animator.SetBool("Attacking", false);
+            Debug.Log("Bruh");
+        }else
+        {
+            Animator.SetBool("Attacking", true);
+        }
+
+            
+
+        
+    }
+
+}
