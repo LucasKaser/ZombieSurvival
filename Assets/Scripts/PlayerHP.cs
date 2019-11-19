6b4f419242328c30d9ac679e075200efd90c27c6
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2019-11-19 13:00:07 -0800

    added health region

--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -5,23 +5,74 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHP : MonoBehaviour
 {
-    public int health = 5;
+    public float health = 15;
     public GameObject Enemy;
+    public float wait;
+    public float waiting;
+    public int healthre;
+    
+    
 
-    private void OnTriggerEnter(Collider other)
+  
+    private void Start()
     {
-        if(gameObject == Enemy)
+        healthre = 3;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Zombie" && wait>= 0.3)
         {
+            wait = 0;
             health--;
+            waiting = 0;
+            if (health <= 0)
+            {
+                SceneManager.LoadScene("Preston Test Level");
+            }
+
+          
+
+
+
         }
     }
 
-    private void OnCollisionStay(Collision collision)
-   {
-        if(gameObject == Enemy)
-        {
-            health--;
-        }
-    }
+  
+       
 
+
+
+    private void Update()
+    {
+        wait += Time.deltaTime;
+        waiting += Time.deltaTime;
+
+        if (waiting > 5)
+        {
+
+
+            if (health <= 15)
+            {
+                health += .05f;
+            }
+            if (health > 15)
+            {
+                
+
+                health = 15;
+            }
+        }
+
+      
+      
+
+
+
+
+
+
+    }
+    
+   
 }
