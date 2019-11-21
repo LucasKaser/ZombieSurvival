9d70dc513aa1f8d3e72b9073bc4222fc629de359
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2019-11-21 11:55:35 -0800

    pushing to prevent merge

---
+++ Assets/Scripts/Tommy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tommy : MonoBehaviour { 
+
+    private SimpleShoot simpleShoot;
+    private OVRGrabbable ovrgrabbable;
+    public OVRInput.Button shootingbutton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        simpleShoot = GetComponent<SimpleShoot>();
+        ovrgrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ovrgrabbable.isGrabbed && OVRInput.GetDown(shootingbutton, ovrgrabbable.grabbedBy.GetController()))
+        {
+            simpleShoot.TriggerShoot();
+        }
+        
+    }
+}
