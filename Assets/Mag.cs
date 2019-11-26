00d51f014c6bacbd893fdb736b73895612f3ebba
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2019-11-26 12:09:10 -0800

    added two scripts

---
+++ Assets/Mag.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mag : MonoBehaviour
+{
+    public int roundsLeft;
+
+
+    public MagSlot slot;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<MagSlot>() != null)
+        {
+            other.GetComponent<MagSlot>().AttachMag(this);
+            GetComponent<BoxCollider>().enabled = false;
+
+            GetComponent<OVRGrabbable>().grabbedBy.gameObject.GetComponent<OVRGrabber>().ForceRelease(gameObject.GetComponent<OVRGrabbable>());
+            transform.parent = slot.transform.Find("AttachPoint");
+            transform.localPosition = Vector3.zero;
+            transform.localEulerAngles = Vector3.zero;
+
+        }
+    }
+    public void PickUp()
+    {
+        if (slot != null)
+        {
+            slot.DetachMag();
+        }
+        GetComponent<OVRGrabbable>();
+    }
+
+    public void Drop ()
+    {
+        if(slot == null)
+        {
+            GetComponent<BoxCollider>().enabled = true;
+            GetComponent<OVRGrabbable>().grabbedBy.gameObject.GetComponent<OVRGrabber>().ForceRelease(gameObject.GetComponent<OVRGrabbable>());
+        }
+    }
+  
+}
