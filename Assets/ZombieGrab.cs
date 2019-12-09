5c9309be1a4175f66832029c3e509b2cfd9e3ced
John Kaser <20.john.kaser@ksd.org>
2019-12-09 11:47:43 -0800

    pushing

---
+++ Assets/ZombieGrab.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ZombieGrab : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Zombie;
+    public OVRGrabbable ovrgrabbable;
+    public OVRInput.Button grabbutton;
+    public BoxCollider collide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrgrabbable = GetComponent<OVRGrabbable>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ovrgrabbable.isGrabbed)
+        {
+            Zombie.GetComponent<Animator>().enabled = false;
+            collide.enabled = false;
+
+        }
+        else if(ovrgrabbable.isGrabbed && OVRInput.GetUp(grabbutton))
+        {
+            ovrgrabbable.grabbedBy.gameObject.GetComponent<OVRGrabber>().ForceRelease(gameObject.GetComponent<OVRGrabbable>());
+            Zombie.GetComponent<Animator>().enabled = true;
+            collide.enabled = true;
+
+        }
+    }
+}
