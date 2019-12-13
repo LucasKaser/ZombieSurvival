b76d3d12d4f34f422cca6ea7a9d9b26ae8c42a4b
John Kaser <20.john.kaser@ksd.org>
2019-12-13 12:11:40 -0800

    wondergun

---
+++ Assets/Scripts/Wondergun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wondergun : MonoBehaviour
+{
+    public GameObject zapper;
+    private OVRGrabbable ovrgrabbable;
+    public OVRInput.Button shootingbutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrgrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ovrgrabbable.isGrabbed && OVRInput.GetDown(shootingbutton, ovrgrabbable.grabbedBy.GetController()))
+        {
+            zapper.SetActive(true);
+        }
+        if (ovrgrabbable.isGrabbed && OVRInput.GetDown(shootingbutton, ovrgrabbable.grabbedBy.GetController()))
+        {
+            zapper.SetActive(false);
+        }
+    }
+}
