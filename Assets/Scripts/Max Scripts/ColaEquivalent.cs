e1b142c671411633a9f52fea3da7c352462d9400
Maxwell Bake <20.maxwell.dickes@ksd.org>
2019-12-13 12:16:20 -0800

    concepts for perks and upgrades

--- Assets/Scripts/Max Scripts/ColaEquivalent.cs
+++ Assets/Scripts/Max Scripts/ColaEquivalent.cs
@@ -19,3 +19,33 @@ public class ColaEquivalent : MonoBehaviour
         
     }
 }
+
+
+//Double damage
+    //the zombie has a set health, different parts of the body might add a multiplyer to the damage you do
+    //the double damage perk would make it so you multiplied all damage after body part multipliers
+//double health
+    //pretty straight forward. doubles health
+//speed/sprint speed increase
+    //adds a small multiplier to the movement speed
+//pool cap increase (larger total ammo pool)                            =======
+    //gets every gun (probably a tag) and doubles their pool cap
+//ammo max increase (larger clips)
+    //gets every gun (probably a tag) and doubles their ammo max. 
+    //this might not exist or might be a part of pool cap**             =======
+//extra gun
+    //trigger on back that if you release gun it snaps to back.
+    //can grab it just by reaching and grabbing anywhere on the back area.
+//
+
+
+
+//pack-a-punch equivalent
+
+//upgrades dmg and ammo caps.
+//able to upgrade all guns (more than once with each price getting significantly higher)
+//able to upgrade cola equivalents (more than once with each price getting significantly higher)
+//item or location
+    //item-found after large round completions and you just have to touch it to the gun/item you wish to upgrade
+    //location-hard to reach spot (small ritual to get there or just dangerous) and you just have to drop the item into the slot to upgrade (weird for cola equivalent) (spits it out if you dont have enough points)
+//
