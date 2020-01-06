83e42e696dea33ac3faf21560fb1909ef2da2d2f
Maxwell Bake <20.maxwell.dickes@ksd.org>
2020-01-06 12:49:01 -0800

    script work for powerups

---
+++ Assets/Scripts/referenceScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class referenceScript : MonoBehaviour
+{
+    public int dmgMult = 1;
+    public bool instaKill = false;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dmgMult = 1;
+    }
+}
